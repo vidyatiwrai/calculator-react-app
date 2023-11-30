ffacca98bc4ea9b9b4978375db7c4a0dad4a9c33
unknown <tiwarividya600@gmail.com>
2023-12-01 02:14:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// App.js
+import React from 'react';
+import './App.css';
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>REACT CALCULATOR</h1>
+      </header>
+      <main>
+        <Calculator />
+      </main>
+    </div>
+  );
+}
+
+export default App;
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,83 +1,46 @@
+// Calculator.js
 import React, { useState } from 'react';
+import './App.css'; // Import the stylesheet
 
-function App() {
-  const [num1, setNum1] = useState('');
-  const [num2, setNum2] = useState('');
+const Calculator = () => {
+  const [input, setInput] = useState(0);
   const [result, setResult] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleNum1Change = (event) => {
-    setNum1(event.target.value);
-  };
-
-  const handleNum2Change = (event) => {
-    setNum2(event.target.value);
-  };
-
-  const validateInputs = () => {
-    if (num1 === '' || num2 === '') {
-      setErrorMessage('Error');
-      return false;
-    }
-    if (!/^-?\d*\.?\d+$/.test(num1) || !/^-?\d*\.?\d+$/.test(num2)) {
-      setErrorMessage('Invalid number format');
-      return false;
-    }
-    setErrorMessage('');
-    return true;
-  };
-
-  const handleOperation = (operation) => {
-    if (validateInputs()) {
-      const number1 = parseFloat(num1);
-      const number2 = parseFloat(num2);
-
-      switch (operation) {
-        case 'add':
-          setResult(number1 + number2);
-          break;
-        case 'subtract':
-          setResult(number1 - number2);
-          break;
-        case 'multiply':
-          setResult(number1 * number2);
-          break;
-        case 'divide':
-       
-          setResult(number1 / number2);
-          break;
-        default:
-          break;
+  const handleButtonClick = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input).toString());
+      } catch (error) {
+        setResult('Error');
       }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput(input + value);
     }
   };
+
+  
 
   return (
-   <center>
-     <div className="container">
-      <h1>React Calculator</h1>
-      <div className="input-container">
-        <input type="text" placeholder="Num 1" value={num1} onChange={handleNum1Change} /><br/><br/>
-        <input type="text" placeholder="Num 2" value={num2} onChange={handleNum2Change} />
-      </div><br/>
-      <div className="buttons">
-        <button onClick={() => handleOperation('add')}>+</button>
-        <button onClick={() => handleOperation('subtract')}>-</button>
-        <button onClick={() => handleOperation('multiply')}>*</button>
-        <button onClick={() => handleOperation('divide')}>/</button>
+    
+    <div className="calculator">
+      <div className="display">
+        <div className="input">{input}</div>
+        <div className="result">{result}</div>
       </div>
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
-      {result !== '' && (
-         <div>
-         <p className="success-message">Success!</p>
-         <p className="result-message">Result: {result}</p>
-       </div>
-
-      )}
-
+      <div className="buttons">
+        {['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+', 'C'].map((button) => (
+          <button key={button} onClick={() => handleButtonClick(button)}>
+            {button}
+          </button>
+        ))}
+      </div>
+      <button className="mode-toggle">
+      </button>
     </div>
-   </center>
   );
-}
+};
 
-export default App;
+export default Calculator;
